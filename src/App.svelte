2c96d29ebf82bc6e91fb97bd57bf9cba165ce25f
Linh M. Tran <linhtm@vietnamlab.vn>
2020-06-30 12:44:11 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import timer from './timer';
+  import Header from './Header.svelte';
+
+  export let name: string;
+
+  let a = 42;
+  let doubled: number;
+  let doubleTimer: number;
+  $: doubled = a * 2;
+  $: doubleTimer = $timer * 2;
+</script>
+
+<main>
+  <Header />
+  <h1>Hello {name}!</h1>
+  <h2>Count is {$timer}</h2>
+  <p>Test {doubled}</p>
+  <p>Test {doubleTimer}</p>
+</main>
+
+<style lang="scss">
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  const test = 42;
+</script>
+
+<h1>Hello World {test}</h1>
